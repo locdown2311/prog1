103d0a42bb3fdd7f44ea31a0c8a428e55453d7ea
Igor Carvalho <igor2311@gmail.com>
2016-02-12 11:10:37 -0200

    Create detrazprafrente.c

---
+++ vetores/detrazprafrente.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+    char st[40],stInv[40];
+    int i, j ,tam= 0;
+    printf("Digite a string :");
+    scanf("%s",st);
+
+    while( st[tam]!= '\0' && tam<40){
+        tam++;
+    }
+    stInv[tam] ='\0';
+    j= tam-1;
+    i = 0;
+    while(i<tam){
+        stInv[j] = st[i];
+        i++;
+        j--;
+
+    }
+    printf("Inversa : %s\n",stInv);
+
+
+}
+
