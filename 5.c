9c35f799e1018a412f007678b9dda361bd64b236
Igor Carvalho Guimarães <igor2311@gmail.com>
2016-02-25 17:57:52 -0500

    Added files via upload

---
+++ 5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+    void nao_troca(int a , int b){
+        int temp;
+        temp =a;
+        a =b;
+        b=temp;
+
+    }
+
+
+void main(){
+    int x =1 , y=2;
+    nao_troca(x,y);
+    printf("Valor de x = [%d] y = [%d]",x,y);
+
+}
