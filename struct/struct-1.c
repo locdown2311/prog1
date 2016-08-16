e2bd41977d064b2e969b01aeb472ccf6f7eedf6a
Igor Carvalho Guimarães <igor2311@gmail.com>
2016-08-15 22:03:08 -0300

    Estrutura de cadastro de alunos

--- struct/struct-1.c
+++ struct/struct-1.c
@@ -2,6 +2,25 @@
 #include <stdio.h>
 
 int main()
-{
+  {
+  struct aluno{
+    char nome[100];
+    int idade;
+  }
+  int n;
+    printf("Digite a quantidade de alunos : ");
+    scanf("%d",&n);
+    
+  struct aluno aluno[n];
   
+  for(i=0;i<n;i++){
+    printf("Digite o nome do aluno : "\n);
+    scanf(" %s",&aluno[i].nome);
+    fflush(stdin);
+    
+    printf("\nDigite a idade do aluno : ");
+    scanf(" %d",&aluno[i].idade);
+  }
+  
+  return 0;
 }
